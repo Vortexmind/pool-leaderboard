fc97819949c55408748f4c04ca3a4fd5912e4b7e
Paolo Tagliaferri <vortexmind@gmail.com>
2015-07-25 17:39:33 +0100

    Add dynamic tasks storage and additions

--- pool-leaderboard.js
+++ pool-leaderboard.js
@@ -1,3 +1,5 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -16,11 +18,28 @@ if (Meteor.isClient) {
   });
   
  Template.taskstp.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function() { 
+		return Tasks.find();
+	}
+  });
+  
+ Template.addtask.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
+    }
   });
   
 }

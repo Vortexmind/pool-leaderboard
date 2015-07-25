e84f30bfc05b67a8edefefd7bc3cc86c152f5e46
Paolo Tagliaferri <vortexmind@gmail.com>
2015-07-25 18:03:09 +0100

    Also remove unused controller 'hello' code

--- pool-leaderboard.js
+++ pool-leaderboard.js
@@ -1,22 +1,7 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-  
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-  
+ 
  Template.taskstp.helpers({
     tasks: function() { 
 		return Tasks.find();

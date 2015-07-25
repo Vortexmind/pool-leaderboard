02081703799ef0ea537ea0b7fb5bf714437fd941
Paolo Tagliaferri <vortexmind@gmail.com>
2015-07-25 19:28:27 +0100

    Updated app with accounts, filters and additional information

--- pool-leaderboard.js
+++ pool-leaderboard.js
@@ -1,11 +1,27 @@
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
  
  Template.taskstp.helpers({
-    tasks: function() { 
-	  return Tasks.find({}, {sort: {createdAt: -1}});
-	}
+    tasks: function () {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
+    }
+
   });
   
  Template.addtask.events({
@@ -19,12 +35,16 @@ if (Meteor.isClient) {
       // Insert a task into the collection
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(), // current time
+		owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
  
       // Clear form
       event.target.text.value = "";
-    }
+    },"change .hide-completed input": function (event) {
+		Session.set("hideCompleted", event.target.checked);
+	}
   });
   
   Template.task.events({
